d4fd624f67ae06e5bb327b08b4f0eb044d5d9868
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-26 13:12:07 +0900

    feat : 라우터 세팅

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavBar(props) {
+    return (
+        <div>네브바</div>
+    );
+}
+
+export default NavBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from "../routes/Home";
+import Board from "../routes/Board";
+import Login from "../routes/Login";
+import Mypage from "../routes/Mypage";
+import Register from "../routes/Register";
+import NavBar from "./NavBar/NavBar";
 
 function App() {
-  return <div>사이드 메이트</div>;
+    return (
+        <BrowserRouter>
+            <Switch>
+                <>
+                    <NavBar/>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/Board" component={Board}/>
+                    <Route exact path="/Mypage" component={Mypage}/>
+                    <Route exact path="/Login" component={Login}/>
+                    <Route exact path="/Register" component={Register}/>
+                </>
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App;
