a65ffcb1181b7ed39ec6149b8c71a5cf01602355
HoJin9622 <kiss0104040@gmail.com>
2020-09-30 12:27:11 +0900

    feat: font & 전역 스타일 지정

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,50 @@
 import React from "react";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import "../styles/index.css";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Home from "../routes/Home";
 import Board from "../routes/Board";
 import Login from "../routes/Login";
+import NavBar from "./NavBar/NavBar";
 import Mypage from "../routes/Mypage";
 import Register from "../routes/Register";
-import NavBar from "./NavBar/NavBar";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 맨발의청춘R !important;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+        cursor: pointer;
+    }
+    ol, ul, li {
+        list-style: none;
+    }
+`;
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <>
-                    <NavBar/>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/Board" component={Board}/>
-                    <Route exact path="/Mypage" component={Mypage}/>
-                    <Route exact path="/Login" component={Login}/>
-                    <Route exact path="/Register" component={Register}/>
-                </>
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <>
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Board" component={Board} />
+          <Route exact path="/Mypage" component={Mypage} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Register" component={Register} />
+        </>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
