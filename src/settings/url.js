1d85ac631048c5cc67c3caf4c00af8adeb5eda6c
kimja01 <kimja7045@naver.com>
2020-10-01 20:12:15 +0900

    feat(url): Update production url

--- src/settings/url.js
+++ src/settings/url.js
@@ -3,8 +3,8 @@ let url = "http://localhost";
 // url = 'http://www.side-mate.com';
 
 if (process.env.NODE_ENV === 'production') {
-    url = "http://52.78.44.143";
-  // url = 'http://www.side-mate.com';
+    // url = "http://52.78.44.143";
+    url = 'http://www.side-mate.com';
 }
 
 export const BASE_URL = url+':8000/api/v1/';
