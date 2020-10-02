bb204d4fb9b3253bcbd1924c3ae1cf32331b1f57
HoJin9622 <kiss0104040@gmail.com>
2020-10-03 00:26:47 +0900

    feat: 회원가입 페이지 스타일 작업

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,8 +1,56 @@
 import React, { useState } from "react";
+import utils from "../utils";
+import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { register } from "../modules/auth";
-import utils from "../utils";
 import { createMessage } from "../modules/messages";
+
+const Container = styled.div`
+  width: 100%;
+  margin-top: 3rem;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 426px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InputContainer = styled.div`
+  width: 90%;
+  margin-bottom: 24px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+  border: 1px solid #8692a6;
+  border-radius: 6px;
+  font-size: 0.875rem;
+`;
+
+const Button = styled.button`
+  width: 90%;
+  height: 64px;
+  border: none;
+  background-color: #1565d8;
+  border-radius: 6px;
+`;
+
+const LoginLabel = styled.span`
+  color: #ffffff;
+  font-size: 1rem;
+`;
+
+const TextLabel = styled.div`
+  color: #696f79;
+  font-size: 1rem;
+  margin-bottom: 0.875rem;
+`;
 
 function Register() {
   const dispatch = useDispatch();
@@ -52,27 +100,37 @@ function Register() {
   };
 
   return (
-    <div>
-      <div>아이디</div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.currentTarget.value)}
-      />
-      <div>닉네임</div>
-      <input
-        type="text"
-        value={nickname}
-        onChange={(e) => setNickname(e.currentTarget.value)}
-      />
-      <div>패스워드</div>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.currentTarget.value)}
-      />
-      <button onClick={onSubmit}>회원가입</button>
-    </div>
+    <Container>
+      <Form>
+        <InputContainer>
+          <TextLabel>아이디</TextLabel>
+          <Input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <TextLabel>닉네임</TextLabel>
+          <Input
+            type="text"
+            value={nickname}
+            onChange={(e) => setNickname(e.currentTarget.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <TextLabel>패스워드</TextLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+        </InputContainer>
+        <Button onClick={onSubmit}>
+          <LoginLabel>회원가입</LoginLabel>
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
