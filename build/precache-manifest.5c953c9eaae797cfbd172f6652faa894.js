419bde2223d1b7d06ff8fe4858c2c97220be9bc9
kimja01 <kimja7045@naver.com>
2020-11-05 22:42:49 +0900

    Add build repo

---
+++ build/precache-manifest.5c953c9eaae797cfbd172f6652faa894.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128697b402829901b812d01c4efdb19f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693ef54504aaa78dbb16",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "3c7e83a45ac044257aee",
+    "url": "/static/css/main.ce2b5102.chunk.css"
+  },
+  {
+    "revision": "693ef54504aaa78dbb16",
+    "url": "/static/js/2.84d6f7aa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.84d6f7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7e83a45ac044257aee",
+    "url": "/static/js/main.282223c8.chunk.js"
+  },
+  {
+    "revision": "1f578bfb6d24ac193c06",
+    "url": "/static/js/runtime-main.5b3608e6.js"
+  },
+  {
+    "revision": "36cc31eca8e63ebe4f26e571546f603c",
+    "url": "/static/media/footB.36cc31ec.ttf"
+  },
+  {
+    "revision": "60ab85363038f7b22c26fe0860153b39",
+    "url": "/static/media/footL.60ab8536.ttf"
+  },
+  {
+    "revision": "89a37d1d418718e96c6f3d9fd2923c15",
+    "url": "/static/media/footR.89a37d1d.ttf"
+  }
+]);
