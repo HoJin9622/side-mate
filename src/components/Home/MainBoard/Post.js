895216757996dc26048558fe1ef79dd0c4ee2c70
kimja01 <kimja7045@naver.com>
2020-11-27 02:26:45 +0900

    Update Post FavbtnStatus

--- src/components/Home/MainBoard/Post.js
+++ src/components/Home/MainBoard/Post.js
@@ -4,6 +4,7 @@ import BookmarkIcon from "@material-ui/icons/Bookmark";
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import { useHistory } from "react-router-dom";
 import api from "../../../settings/api";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   width: 345px;
@@ -17,6 +18,7 @@ const Container = styled.div`
 function Post({ item }) {
     const history = useHistory();
     const [post, setPost] = useState(item);
+    const { isAuthenticated } = useSelector((state) => state.auth);
 
     const onChangeFavorite = (hasFavorite) => {
         api.get(
@@ -59,23 +61,26 @@ function Post({ item }) {
                     >
                         <img
                             alt=""
-                            src="https://placeimg.com/60/60/anys"
+                            src="https://placeimg.com/60/60/animals"
                             style={{
                                 borderRadius: "50%",
                                 marginRight: 10,
                             }}
                         />
-                        <p
-                            style={{
-                                fontSize: 20,
-                                lineHeight: 26,
-                                fontWeight: "bold",
-                            }}
-                        >
-                            {post.user.nickname}
-                        </p>
+                        <div>
+                            <p
+                                style={{
+                                    fontSize: 20,
+                                    lineHeight: 26,
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                {post.user?.nickname}
+                            </p>
+                        </div>
                     </div>
                     <div>
+                        {isAuthenticated &&
                         <button
                             style={{border: 'none', backgroundColor: '#fff', zIndex: 100}}
                             onClick={()=>onChangeFavorite(post.has_favorite)}
@@ -85,7 +90,7 @@ function Post({ item }) {
                                 :
                                 <BookmarkBorderIcon/>
                             }
-                        </button>
+                        </button>}
 
                     </div>
                 </div>
